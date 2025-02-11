3849d3adf7b42ed3830803199bffc39b0a4f5982
Ariony92 <zkhan@iut.univ-paris8.fr>
2025-02-11 14:05:09 +0100

    A faire: les cartes

---
+++ TpAngularSite/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { HomeMainComponent } from './Hero/home-main.component';
+import { CartesComponent } from './cartes/cartes.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, HomeMainComponent, CartesComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.sass'
+})
+export class AppComponent {
+  title = 'TpAngularSite';
+}
