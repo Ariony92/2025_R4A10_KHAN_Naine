54fd775948532b20fa8c4822f8552cd7b37406e4
Ariony92 <zkhan@iut.univ-paris8.fr>
2025-02-14 13:29:58 +0100

    continuité cartes

--- TpAngularSite/src/app/cartes/grid/grid.component.ts
+++ TpAngularSite/src/app/cartes/grid/grid.component.ts
@@ -24,9 +24,30 @@ export class GridComponent {
     image: "assets/loop.png",
   },
     {
-      title: "ouiiiii",
-      description: "",
-      image: ""
+      title: "Online pharmacy",
+      description: "Buy your medicines with our mobile application with a simple delivery system",
+      image: "assets/bocalmedoc.png",
+    },
+    {
+      title: "Consultation",
+      description: "Free consultation with our trusted doctors and get the best recomendations",
+      image: "assets/bocalmedoc.png",
+    },
+    {
+      title: "Details info",
+      description: "Free consultation with our trusted doctors and get the best recomendations",
+      image: "assets/stats.png",
+    },
+    {
+      title: "Emergency care",
+      description: "You can get 24/7 urgent care for yourself or your children and your\n" +
+        "lovely family",
+      image: "assets/trousse.png",
+    },
+    {
+      title: "Tracking",
+      description: "Track and save your medical history and health data ",
+      image: "assets/tab.png",
     },
 
   ]
