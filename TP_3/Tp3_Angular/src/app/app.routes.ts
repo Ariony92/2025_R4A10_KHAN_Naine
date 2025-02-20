be2ff98948f9fdb0c20a78ba807a3ef25f9a4a36
Ariony92 <zkhan@iut.univ-paris8.fr>
2025-02-20 13:39:55 +0100

    route home -> voyage réalisé

--- TP_3/Tp3_Angular/src/app/app.routes.ts
+++ TP_3/Tp3_Angular/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
+import {VoyagesComponent} from "./voyages/voyages.component";
 
 export const routes: Routes = [
   {
@@ -10,6 +11,10 @@ export const routes: Routes = [
   {
     path: 'home',
     redirectTo: '',
+  },
+  {
+    path: 'voyages',
+    component: VoyagesComponent,
   },
   {
     path: '**',
